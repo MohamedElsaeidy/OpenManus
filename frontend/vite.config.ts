1ad7e399d214a2106f0d81265ab6cfb60776c4f6
MohamedElsaeidy <mohamed@bahadir>
2026-05-15 01:03:54 +0300

    chore: update path alias in vite.config.ts to use node:path for absolute resolution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
