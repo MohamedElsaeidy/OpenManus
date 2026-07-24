fe56eb9852fe1f4807e6b50dd4fc1c93129b59a3
MohamedElsaeidy <mohamed@bahadir>
2026-07-24 04:01:23 +0300

    refactor: streamline CI/CD by consolidating deployment workflows and removing legacy smoke tests

--- tests/test_task_event_stream.py
+++ tests/test_task_event_stream.py
@@ -2,9 +2,7 @@ from server.api.routers.tasks import _stream_created_at
 
 
 def test_stream_created_at_converts_redis_timestamp():
-    assert _stream_created_at("1784204897722-0") == (
-        "2026-07-16T12:28:17.722000+00:00"
-    )
+    assert _stream_created_at("1784204897722-0") == ("2026-07-16T12:28:17.722000+00:00")
 
 
 def test_stream_created_at_rejects_invalid_stream_id():
